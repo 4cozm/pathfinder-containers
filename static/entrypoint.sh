f0bffb4d87fd75ec47cb4e4d4d231be868569e98
4cozm <17328rm@gmail.com>
2026-03-07 19:48:56 +0900

    feat: Add Discord webhook configuration to environment setup

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 crontab /var/crontab.txt
 envsubst '$DOMAIN' </etc/nginx/templateSite.conf >/etc/nginx/sites_enabled/site.conf
 envsubst '$PATHFINDER_SOCKET_HOST' </etc/nginx/templateNginx.conf >/etc/nginx/nginx.conf
-envsubst '${DOMAIN} ${CCP_SSO_CLIENT_ID} ${CCP_SSO_SECRET_KEY} ${CCP_ESI_SCOPES} ${PATHFINDER_SOCKET_HOST} ${PATHFINDER_SOCKET_PORT} ${MYSQL_HOST} ${MYSQL_PORT} ${MYSQL_USER} ${MYSQL_PASSWORD} ${MYSQL_PF_DB_NAME} ${MYSQL_UNIVERSE_DB_NAME} ${MYSQL_CCP_DB_NAME} ${PF_STANDALONE_SECRET}' \
+envsubst '${DOMAIN} ${CCP_SSO_CLIENT_ID} ${CCP_SSO_SECRET_KEY} ${CCP_ESI_SCOPES} ${PATHFINDER_SOCKET_HOST} ${PATHFINDER_SOCKET_PORT} ${MYSQL_HOST} ${MYSQL_PORT} ${MYSQL_USER} ${MYSQL_PASSWORD} ${MYSQL_PF_DB_NAME} ${MYSQL_UNIVERSE_DB_NAME} ${MYSQL_CCP_DB_NAME} ${PF_STANDALONE_SECRET} ${DISCORD_WEBHOOK_IT_PING} ${DISCORD_ALERT_WEBHOOK_URL}' \
   </var/www/html/pathfinder/app/templateEnvironment.ini \
   >/var/www/html/pathfinder/app/environment.ini
 
